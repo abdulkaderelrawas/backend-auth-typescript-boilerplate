1f2840b4d8e929798a8ab9f388900cb606b29729
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2020-11-10 19:24:53 +0300

    Generate JsonWebToken

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,16 @@
 import User from '../models/UserModel';
+import { GenerateToken } from '../utils/GenerateToken';
 
 export class UserController {
+  //          d8888      8888888b.       888b     d888      8888888      888b    888
+  //       d88888      888  "Y88b      8888b   d8888        888        8888b   888
+  //      d88P888      888    888      88888b.d88888        888        88888b  888
+  //     d88P 888      888    888      888Y88888P888        888        888Y88b 888
+  //    d88P  888      888    888      888 Y888P 888        888        888 Y88b888
+  //   d88P   888      888    888      888  Y8P  888        888        888  Y88888
+  //  d8888888888      888  .d88P      888   "   888        888        888   Y8888
+  // d88P     888      8888888P"       888       888      8888888      888    Y888
+
   /**
    * @description   GET all users
    * @route         GET /api/v1/users
@@ -16,6 +26,24 @@ export class UserController {
       }
     });
   }
+
+  //   8888888b.       8888888b.        .d88888b.       88888888888      8888888888       .d8888b.       88888888888
+  // 888   Y88b      888   Y88b      d88P" "Y88b          888          888             d88P  Y88b          888
+  // 888    888      888    888      888     888          888          888             888    888          888
+  // 888   d88P      888   d88P      888     888          888          8888888         888                 888
+  // 8888888P"       8888888P"       888     888          888          888             888                 888
+  // 888             888 T88b        888     888          888          888             888    888          888
+  // 888             888  T88b       Y88b. .d88P          888          888             Y88b  d88P          888
+  // 888             888   T88b       "Y88888P"           888          8888888888       "Y8888P"           888
+
+  //   8888888b.       888     888      888888b.        888           8888888       .d8888b.
+  // 888   Y88b      888     888      888  "88b       888             888        d88P  Y88b
+  // 888    888      888     888      888  .88P       888             888        888    888
+  // 888   d88P      888     888      8888888K.       888             888        888
+  // 8888888P"       888     888      888  "Y88b      888             888        888
+  // 888             888     888      888    888      888             888        888    888
+  // 888             Y88b. .d88P      888   d88P      888             888        Y88b  d88P
+  // 888              "Y88888P"       8888888P"       88888888      8888888       "Y8888P"
 
   /**
    * @description   Resgister a new user
@@ -46,13 +74,16 @@ export class UserController {
         });
 
         if (user) {
-          resolve({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: 'xxxx',
-          });
+          const jwt = new GenerateToken(user._id);
+          if (jwt) {
+            resolve({
+              _id: user._id,
+              name: user.name,
+              email: user.email,
+              isAdmin: user.isAdmin,
+              token: await jwt.generateToken(),
+            });
+          }
         } else {
           return reject('Invalid user data.');
         }
---
+++ src/utils/GenerateToken.ts
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+export class GenerateToken {
+  private static readonly JWT_SECRET = process.env.JWT_SECRET;
+  private userID: string;
+
+  constructor(userID: string) {
+    this.userID = userID;
+  }
+
+  public async generateToken(): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const userID = this.userID;
+        const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
+          expiresIn: '1d',
+        });
+
+        resolve(token);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+}
